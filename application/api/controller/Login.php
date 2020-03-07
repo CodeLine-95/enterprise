26b656483815f00e056b3f3339e46f39a0eeb881
qiaoshuai <13295518684@163.com>
2020-03-07 16:42:59 +0800

    新增：修改api

--- application/api/controller/Login.php
+++ application/api/controller/Login.php
@@ -62,6 +62,7 @@ class Login extends Controller
             if (request()->isPost()) {
                 $params = request()->post();
                 $params['user_pwd'] = cms_pwd_encode($params['user_pwd']);
+                $params['user_nick'] = 'UID_'.randStr(10);
                 $params['user_host'] = request()->ip();
                 $params['create_time'] = time();
                 $res = (new Users())->where('user_name', $params['user_name'])->find();
@@ -75,6 +76,66 @@ class Login extends Controller
                     }
                 }
             } else {
+                return json(['codeMsg' => '请求错误', 'code' => 400]);
+            }
+        }catch (\Exception $e){
+            Log::error($e);
+            return json(['codeMsg'=>$e->getMessage(),'code'=>$e->getCode()]);
+        }
+    }
+
+    /**
+     * 修改密码  [post]
+     * @param $user_pwd string 旧密码
+     * @param $re_user_pwd string 新密码
+     * @return \think\response\Json
+     */
+    public function update_pwd(){
+        try {
+            if(request()->isPost()){
+                $params = request()->post();
+                $user = (new Users())->where(['id'=>$params['uid']])->find();
+                if (!$user){
+                    return json(['codeMsg'=>'没有该用户','code'=>401]);
+                }
+                if(!cms_pwd_verify($params['user_pwd'],$user['user_pwd'])){
+                    return json(['codeMsg'=>'旧密码错误','code'=>402]);
+                }
+                $re_pass =  cms_pwd_encode($params['re_user_pwd']);
+                $re = (new Users())->where('id',$params['uid'])->update(['user_pwd'=>$re_pass]);
+                if($re){
+                    return json(['codeMsg'=>'SUCCESS','code'=>200]);
+                }else{
+                    return json(['codeMsg'=>"error",'code'=>400]);
+                }
+            }else{
+                return json(['codeMsg' => '请求错误', 'code' => 400]);
+            }
+        }catch (\Exception $e){
+            Log::error($e);
+            return json(['codeMsg'=>$e->getMessage(),'code'=>$e->getCode()]);
+        }
+    }
+
+    /**
+     * 修改信息 [post]
+     * @return \think\response\Json
+     */
+    public function update_user(){
+        try {
+            if (request()->isPost()){
+                $params = request()->post();
+                $user = (new Users())->where(['id'=>$params['uid']])->find();
+                if (!$user){
+                    return json(['codeMsg'=>'没有该用户','code'=>401]);
+                }
+                $re = (new Users())->update($params);
+                if($re){
+                    return json(['codeMsg'=>'SUCCESS','code'=>200]);
+                }else{
+                    return json(['codeMsg'=>"error",'code'=>400]);
+                }
+            }else{
                 return json(['codeMsg' => '请求错误', 'code' => 400]);
             }
         }catch (\Exception $e){
