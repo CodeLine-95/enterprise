575a77690b023eb19ba8b9bcbf2902101436e3e3
qiaoshuai <13295518684@163.com>
2020-03-07 16:29:41 +0800

    update

--- application/api/behavior/CrossDomain.php
+++ application/api/behavior/CrossDomain.php
@@ -4,19 +4,35 @@ namespace app\api\behavior;
 use think\Response;
 
 class CrossDomain{
-    public function moduleInit(&$params){
-        header('Access-Control-Allow-Origin: *');
+    public function run(&$dispatch){
         $host_name = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "*";
         $headers = [
             "Access-Control-Allow-Origin" => $host_name,
             "Access-Control-Allow-Credentials" => 'true',
-            "Access-Control-Allow-Headers" => "x-token,x-uid,x-token-check,x-requested-with,content-type,Host"
+            "Access-Control-Allow-Headers" => "X-Token,x-uid,x-token-check,x-requested-with,content-type,Host"
         ];
-        if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            $params['type'] = 'response';
-            $response = Response::create()->header($headers);
-            $params['response'] = $response;
+        if($dispatch instanceof Response) {
+            $dispatch->header($headers);
+        } else if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            $dispatch['type'] = 'response';
+            $response = new Response('', 200, $headers);
+            $dispatch['response'] = $response;
         }
-        $params->header = Response::create()->header($headers);
     }
+//    public function moduleInit(&$params){
+//        header('Access-Control-Allow-Origin: *');
+//        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+//        $host_name = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "*";
+//        $headers = [
+//            "Access-Control-Allow-Origin" => $host_name,
+//            "Access-Control-Allow-Credentials" => 'true',
+//            "Access-Control-Allow-Headers" => "x-token,x-uid,x-token-check,x-requested-with,content-type,Host"
+//        ];
+//        if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//            $params['type'] = 'response';
+//            $response = Response::create()->header($headers);
+//            $params['response'] = $response;
+//        }
+//        $params->header = Response::create()->header($headers);
+//    }
 }
