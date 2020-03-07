e9befe19ffe8f845b2c64cf76a55e313cbcc2fe5
qiaoshuai <13295518684@163.com>
2020-03-07 16:30:52 +0800

    update

--- application/api/behavior/CrossDomain.php
+++ application/api/behavior/CrossDomain.php
@@ -19,20 +19,4 @@ class CrossDomain{
             $dispatch['response'] = $response;
         }
     }
-//    public function moduleInit(&$params){
-//        header('Access-Control-Allow-Origin: *');
-//        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-//        $host_name = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "*";
-//        $headers = [
-//            "Access-Control-Allow-Origin" => $host_name,
-//            "Access-Control-Allow-Credentials" => 'true',
-//            "Access-Control-Allow-Headers" => "x-token,x-uid,x-token-check,x-requested-with,content-type,Host"
-//        ];
-//        if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-//            $params['type'] = 'response';
-//            $response = Response::create()->header($headers);
-//            $params['response'] = $response;
-//        }
-//        $params->header = Response::create()->header($headers);
-//    }
 }
